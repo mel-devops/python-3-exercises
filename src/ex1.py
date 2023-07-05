0331e63d3c518d8db934c2c4cccf683afab434b5
Melissa Schaefer <mschaefer@xpanxion.com>
2023-07-05 15:17:55 -0400

    update

--- src/ex1.py
+++ src/ex1.py
@@ -1,15 +1,1 @@
-from ValidationException import ValidationException
-
-
-def validate_file():
-    pass # TODO
-
-
-
-def ex1():
-    try:
-        validate_file("input.txt")
-    except ValidationException as ve:
-        print(ve)
-
-ex1()
+new 
